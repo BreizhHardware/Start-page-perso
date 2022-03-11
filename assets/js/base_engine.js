6ed110b6348a8c4215b5242177402858f87c0d80
BreizhHardware <fpmovie4@gmail.com>
2022-03-11 22:28:32 +0100

    Init

---
+++ assets/js/base_engine.js
@@ -0,0 +1,71 @@
+/*	GOOGLE
+	----------------------------------------------------- */
+
+eng.google = {
+	pageTitle: "Google",
+	logo: "../assets/images/google.png",
+	places: {
+		'Web'    : ["https://www.google.com/search?q=%query%", false],
+		'Images' : ["https://images.google.com/images?q=%query%", false],
+		'Maps'   : ["https://maps.google.com/maps?q=%query%", false]
+	}
+};
+
+
+/*	WIKIPEDIA
+	----------------------------------------------------- */
+	
+eng.wikipedia = {
+	pageTitle: "Wikipedia",
+	logo: "../assets/images/wikipedia.png",
+	places: {
+		'Search'        : ["https://fr.wikipedia.org/wiki/Special:Search?search=%query%&fulltext=Search", false],
+		'Go to Article' : ["https://fr.wikipedia.org/wiki/Special:Search?search=%query%&go=Go", false],
+	}
+};
+
+
+/*	YOUTUBE
+	----------------------------------------------------- */
+
+eng.youtube = {
+	pageTitle: "YouTube",
+	logo: "../assets/images/youtube.png",
+	places: {
+		'Videos' : ["https://www.youtube.com/results?search_query=%query%", false]
+	},
+};
+
+
+/*	TWITTER
+	----------------------------------------------------- */
+
+eng.twitter = {
+	pageTitle: "Twitter",
+	logo: "../assets/images/twitter.png",
+	places: {
+		'Search Twitter' : ["https://twitter.com/search?q=%query%", false]
+	}
+};
+
+/*	Twitch
+	----------------------------------------------------- */
+
+	eng.twicth = {
+		pageTitle: "Twitch",
+		logo: "../assets/images/twitch.png",
+		places: {
+			'Search Twitter' : ["https://www.twitch.tv/search?term=%query%", false]
+		}
+	};
+
+/*	GitHub
+	----------------------------------------------------- */
+
+	eng.github = {
+		pageTitle: "GitHub",
+		logo: "../assets/images/github.png",
+		places: {
+			'Search Twitter' : ["https://github.com/search?q=%query%", false]
+		}
+	};
